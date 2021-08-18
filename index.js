8c8d0d0546ecc5f713d6067fba337b38df00397e
= <39552823+Gerald-Izuchukwu@users.noreply.github.com>
2021-08-18 20:59:17 +0100

    database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+connectDB();
 
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
